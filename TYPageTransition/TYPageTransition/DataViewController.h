aa7cc6385a2623e21316fd99d64cfee4eac74187
tengjiachao <tengjiachao@126.com>
2017-03-05 14:57:40 +0800

    转场动画像翻书一样

---
+++ TYPageTransition/TYPageTransition/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  TYPageTransition
+//
+//  Created by thomasTY on 15/11/15.
+//  Copyright © 2015年 滕佳超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (copy, nonatomic) NSString * dataObject;
+@property(nonatomic,assign)NSUInteger pageNum;
+
+@end
+
---
+++ TYPageTransition/TYPageTransition/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TYPageTransition
+//
+//  Created by thomasTY on 15/11/15.
+//  Copyright © 2015年 滕佳超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
+
